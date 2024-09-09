df8c2c4b7afb5d488a0d3e29da160b0f6a53b5c2
Satish Maharaj <satishlawrencemaharaj@gmal.com>
2024-09-09 00:02:10 -0400

    Route

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -81,7 +81,7 @@ const Navbar = () => {
           </div>
         </div>
 
-        <Link href="/settings">
+        <Link href="/">
           <Settings
             className="cursor-pointer text-gray-500 dark:text-gray-300"
             size={24}
